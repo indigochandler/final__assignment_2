eb75f0331c8bbbd403d88cffde11d691efe668b7
Indigo Chandler <indigochandler@gmail.com>
2025-03-20 14:19:54 +0000

    removing rows which make no sense and removing rows with missing values for T3 and T4.

--- final_assignment_2.py
+++ final_assignment_2.py
@@ -8,3 +8,10 @@ import os
 def load_data(filename, delimiter='\t'):
     return pd.read_csv(filename, delimiter=delimiter)
 
+def handle_missing_values(data):
+    """Removing rows with missing values for T3 and T; for rows where only one value is missing, replace that missing T3 or T4 value with the mean for their Level group."""
+    data = data.dropna(subset=['T3', 'T4'], how='all')
+    data['T3'] = data['T3'].fillna(data.groupby('Level')['T3'].transform('mean')).round(1)
+    data['T4'] = data['T4'].fillna(data.groupby('Level')['T4'].transform('mean')).round(1)
+    return data
+    
